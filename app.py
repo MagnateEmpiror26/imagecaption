16336a8670461991ae21a4710277693da10f629e
MagnateEmpiror26 <62644232+MagnateEmpiror26@users.noreply.github.com>
2023-10-02 10:18:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,8 @@ if(video is not None):
     def predict_caption(image, max_length=10):
         image = Image.open(image)
         pixel_values = IMAGEFEATUREEXTRACTOR(image, return_tensors ="pt").pixel_values
-        pixel_values = tf.convert_to_tensor(pixel_values, dtype=tf.float32)   
+        pixel_values = tf.convert_to_tensor(pixel_values, dtype=tf.float32)
+	pixel_values = pixel_values.reshape(1,1920)
 
         for i in range(max_length):
             sequence = TOKENIZER.texts_to_sequences(["startseq"])[0]
